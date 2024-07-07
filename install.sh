a9a1da3ee6fa28094fe48011207fa368a20dea96
slashpotato <slashpotato@proton.me>
2024-07-07 20:26:14 +0400

    sdkman support

--- install.sh
+++ install.sh
@@ -14,7 +14,14 @@ if [ ! -d $HOME/.oh-my-zsh ]; then
 	cd $DOTFILESDIR
 fi
 
+if [ ! -d $HOME/.sdkman ]; then
+	cd $HOME
+	curl -s "https://get.sdkman.io" | bash
+	cd $DOTFILESDIR
+fi
+
 stow . -t ~ --adopt --ignore=install.sh
 fast-theme XDG:catppuccin-mocha &> /dev/null
 bat cache --build &> /dev/null
 export BAT_THEME="Catppuccin Mocha"
+source "$HOME/.sdkman/bin/sdkman-init.sh"
